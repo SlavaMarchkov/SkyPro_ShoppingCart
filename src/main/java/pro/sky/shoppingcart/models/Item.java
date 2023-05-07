b84a85b328eebbc21cc1a7280499a4cebda0de9a
SlavaMarchkov <slavamarchkov@yandex.ru>
2023-05-07 19:35:58 +0700

    Добавляет реализацию корзины интернет-магазина

---
+++ src/main/java/pro/sky/shoppingcart/models/Item.java
@@ -0,0 +1,24 @@
+package pro.sky.shoppingcart.models;
+
+public class Item {
+    private final int id;
+    private final String name;
+
+    public Item(final int id) {
+        this.id = id;
+        this.name = "name" + id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return "Item: id=" + id + ", name=" + name;
+    }
+}
